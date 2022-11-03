09daa28f33848098bd348feb48f4b0fcae82056a
ricardo <ricardolopez28@gmail.com>
2022-11-03 05:21:11 -0500

    refactor: add foodModelHelper inject contructor FoodModel

--- src/_test_/foodModel.spec.ts
+++ src/_test_/foodModel.spec.ts
@@ -2,7 +2,11 @@ import { FoodModel } from './../models/food-model'
 
 describe('class', () => {
   it('Class FoodModel', () => {
-    const food = new FoodModel('Pescado', 'Proteina', 200)
+    const food = new FoodModel({
+      name: 'Pescado',
+      description: 'Proteina',
+      price: 200
+    })
 
     expect(food.getName()).toEqual('Pescado')
     expect(food.getDescription()).toEqual('Proteina')
